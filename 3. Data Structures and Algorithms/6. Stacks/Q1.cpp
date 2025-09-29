419f1906771beaa581f8060dc1ca1d44e1193490
Asjad-Raza-10 <syedasjadrazashirazi@gmail.com>
2025-09-30 03:08:41 +0500

    Q1. Implemented Checking of Opening/Closing tags and their brackets without attributes.

--- 3. Data Structures and Algorithms/6. Stacks/Q1.cpp
+++ 3. Data Structures and Algorithms/6. Stacks/Q1.cpp
@@ -76,9 +76,10 @@ template <class T> class Stack
 
 int main()
 {
+    Stack<string> tags;
+
     ifstream file(
         "/Volumes/Data/Portfolio/Github/Programming Course/3. Data Structures and Algorithms/6. Stacks/test.xml");
-    // open the file
     if (!file.is_open())
     {
         cout << "Error: Could not open file!" << endl;
@@ -88,10 +89,112 @@ int main()
     string line;
     int lineNumber = 0;
 
-    while (getline(file, line)) // read line by line
+    getline(file, line);
+    int i = 0;
+
+    // header checks
+    if (line.size() < 2 || line[i++] != '<')
     {
+        cerr << "Error: No opening tag for the header" << endl;
+    }
+
+    if (i >= line.size() || line[i++] != '?')
+    {
+        cerr << "Error: Header not defined" << endl;
+    }
+
+    i = line.size() - 1;
+    if (line[i] != '>')
+    {
+        cerr << "Error: No closing tag for the header" << endl;
+    }
+
+    if (i == 0 || line[i - 1] != '?')
+    {
+        cerr << "Error: No closing ? for the header" << endl;
+    }
+
+    // inner string checks
+    while (getline(file, line))
+    {
+        bool openingBracketFound = false;
+        bool closingTagFound = false;
+        string tag = "";
+        i = 0;
+
+        while (i < line.size())
+        {
+            char c = line[i];
+            if (c == '<')
+            {
+                if (openingBracketFound)
+                {
+                    cerr << "Multiple Tags opened before closing the previous one." << endl;
+                }
+                else
+                {
+                    openingBracketFound = true;
+                }
+            }
+            else if (c == '>')
+            {
+                if (openingBracketFound)
+                {
+                    openingBracketFound = false;
+                    if (!tag.empty())
+                    {
+                        if (!closingTagFound)
+                        {
+                            tags.push(tag);
+                        }
+                        else
+                        {
+                            if (tags.isEmpty())
+                            {
+                                cerr << "Error: Closing tag '" << tag << "' without matching opening tag" << endl;
+                            }
+                            else
+                            {
+                                string getTag = tags.pop();
+                                if (tag != getTag)
+                                {
+                                    cerr << getTag << " was opened but a different tag i.e. " << tag
+                                         << " is being closed before the opened one." << endl;
+                                }
+                            }
+                        }
+                    }
+                    tag = "";
+                    closingTagFound = false;
+                }
+                else
+                {
+                    cerr << "Error: Closing Tag doesnot have an opening" << endl;
+                }
+            }
+            else if (openingBracketFound)
+            {
+                if (c == '/')
+                {
+                    closingTagFound = true;
+                }
+                else
+                {
+                    tag += c;
+                }
+            }
+            i++;
+        }
+
         lineNumber++;
-        cout << "Line " << lineNumber << ": " << line << endl;
+        cout << endl << "Line " << lineNumber << ": " << line << endl;
+    }
+
+    // if any tags were not closed
+    while (!tags.isEmpty())
+    {
+        string unclosedTag = tags.pop();
+        cerr << "Error: Closing tag for '" << unclosedTag << "' is missing" << endl;
     }
 
     file.close();
