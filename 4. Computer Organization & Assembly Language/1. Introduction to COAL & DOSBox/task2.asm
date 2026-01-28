661780d2c01113e76af523c3ab897462faf4dfe8
Syed Asjad Raza Shirazi <syedasjadrazashirazi@gmail.com>
2026-01-28 16:57:14 +0500

    Added Introduction to COAL & DOSBox

---
+++ 4. Computer Organization & Assembly Language/1. Introduction to COAL & DOSBox/task2.asm
@@ -0,0 +1,14 @@
+[org 0x0100] ;we will see org directive later
+mov ax, 6
+
+
+add ax, 6
+add ax, 6
+add ax, 6
+add ax, 6
+add ax, 6
+
+
+
+mov ax, 0x4c00 ;terminate the program
+int 0x21
---
+++ 4. Computer Organization & Assembly Language/1. Introduction to COAL & DOSBox/task3.asm
@@ -0,0 +1,14 @@
+[org 0x0100] ;we will see org directive later
+mov al, 6
+
+
+add al, 6
+add al, 6
+add al, 6
+add al, 6
+add al, 6
+
+
+
+mov ax, 0x4c00 ;terminate the program
+int 0x21
